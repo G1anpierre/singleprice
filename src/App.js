de40582048ed4ba5bc2473e3c57fa41a3655d568
Gianpierre Fernandez <fgianpierre@gmail.com>
2020-04-05 07:19:20 +0200

    correct mobile version, by reseting its padding with margins, the same way rows does it with container and adjust heights

--- src/App.js
+++ src/App.js
@@ -8,47 +8,47 @@ const App = props => {
   return (
       <div className="pricing">
         
-      <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="join">
-                <h3 className="join__title">Join our Community</h3>
-                <p className="join__conditions">30-day, hassle-free money back guarantee</p>
-                <p className="join__text">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills</p>
+        <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="join">
+                  <h3 className="join__title">Join our Community</h3>
+                  <p className="join__conditions">30-day, hassle-free money back guarantee</p>
+                  <p className="join__text">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills</p>
+                </div>
               </div>
             </div>
-          </div>
+            
+            <div className="row no-gutters">
+                <div className="col-12 col-md-6">
+                      <div className="subscription">
+                          <h3 className="subscription__title">Monthly Subscription</h3>
+                          <div className="subscription__amount">
+                            <span className="price">$29</span>
+                            <span className="month">per month</span>
+                          </div>
+                          <p className="subscription__text">Full access for less than $1 a day</p>
+                          <button className="subscription__button btn btn-block"> Sign Up</button>
+                      </div>  
+                </div>
+            
+                <div className="col-12 col-md-6">
+                      <div className="whyus"> 
+                        <h3 className="whyus__title">Why Us</h3>
+                        <ul className="whyus__group list-group">
+                          <li>Tutorials by Industry experts</li>
+                          <li>Peer & expert code review</li>
+                          <li>Coding exercises</li>
+                          <li>Access to our Github repos</li>
+                          <li>Community forum</li>
+                          <li>Flashcard decks</li>
+                          <li>New videos every week</li>
+                        </ul>
+                      </div>
+                </div>
+            </div> 
           
-          <div className="row no-gutters">
-              <div className="col-12 col-md-6">
-                    <div className="subscription">
-                        <h3 className="subscription__title">Monthly Subscription</h3>
-                        <div className="subscription__amount">
-                          <span className="price">$29</span>
-                          <span className="month">per month</span>
-                        </div>
-                        <p className="subscription__text">Full access for less than $1 a day</p>
-                        <button className="subscription__button btn btn-block"> Sign Up</button>
-                    </div>  
-              </div>
-          
-              <div className="col-12 col-md-6">
-                    <div className="whyus"> 
-                      <h3 className="whyus__title">Why Us</h3>
-                      <ul className="whyus__group list-group">
-                        <li>Tutorials by Industry experts</li>
-                        <li>Peer & expert code review</li>
-                        <li>Coding exercises</li>
-                        <li>Access to our Github repos</li>
-                        <li>Community forum</li>
-                        <li>Flashcard decks</li>
-                        <li>New videos every week</li>
-                      </ul>
-                    </div>
-              </div>
-          </div> 
-        
-      </div>
+        </div>
       </div>     
   );
 }
